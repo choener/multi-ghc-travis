1ba88abf62d5fb0768d09470140bd4fcab906aca
Christian Hoener zu Siederdissen <choener@bioinf.uni-leipzig.de>
2016-10-04 20:15:45 +0200

    updated travis to deal with new location

--- mk-travis.sh
+++ mk-travis.sh
@@ -2,6 +2,6 @@
 
 # prepare travis file with everything we need
 
-~/Documents/University/multi-ghc-travis/make_travis_yml_2.hs *cabal happy-1.19.5 alex-3.1.7 | \
+~/Documents/University/other/multi-ghc-travis/make_travis_yml_2.hs *cabal happy-1.19.5 alex-3.1.7 | \
 sed 's/\$PATH/\/opt\/happy\/1.19.5\/bin:\/opt\/alex\/3.1.7\/bin:$PATH/' | \
 tee .travis.yml
