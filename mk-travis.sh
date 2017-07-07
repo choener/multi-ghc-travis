3b81427442823e0ded3fdef2e70f4cb7995c07ad
Christian Hoener zu Siederdissen <choener@bioinf.uni-leipzig.de>
2017-07-07 16:52:31 +0200

    updated run script

--- mk-travis.sh
+++ mk-travis.sh
@@ -2,6 +2,10 @@
 
 # prepare travis file with everything we need
 
-~/Documents/University/other/multi-ghc-travis/make_travis_yml_2.hs *cabal happy-1.19.5 alex-3.1.7 | \
-sed 's/\$PATH/\/opt\/happy\/1.19.5\/bin:\/opt\/alex\/3.1.7\/bin:$PATH/' | \
+runghc --ghc-arg="-package Cabal" \
+~/Documents/University/other/multi-ghc-travis/make_travis_yml_2.hs *cabal alex happy | \
 tee .travis.yml
+
+#~/Documents/University/other/multi-ghc-travis/make_travis_yml_2.hs *cabal happy-1.19.5 alex-3.1.7 | \
+#sed 's/\$PATH/\/opt\/happy\/1.19.5\/bin:\/opt\/alex\/3.1.7\/bin:$PATH/' | \
+
